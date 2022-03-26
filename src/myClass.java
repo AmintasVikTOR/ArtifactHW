0cb86d73cfe114e69cb10ee294ca0da2217fdadd
AmintasVikTOR <amintas.vik@gmail.com>
2022-03-26 18:52:52 +0300

    Test first commit

---
+++ src/myClass.java
@@ -0,0 +1,8 @@
+public class myClass {
+    public static void main(String[] args) {
+        int i;
+        i=1;
+        System.out.println(i+"\n"+"Da-Da_da");
+        System.out.println("Hello< AVI");
+    }
+}
